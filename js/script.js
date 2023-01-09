14201405085e5dc14aba6483c4bad4c639fc2b4f
ilario-moreni <ilario2001.mor@gmail.com>
2023-01-09 17:05:29 +0100

    show 10 random email on dom

--- js/script.js
+++ js/script.js
@@ -3,7 +3,15 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
-            message: 'collegato'
+            emailArray: []
+        }
+    },
+    mounted(){
+        for(let i =0; i < 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+                this.emailArray.push(result.data.response)
+                console.log(result.data.response)
+            })
         }
     }
 }).mount('#app')
